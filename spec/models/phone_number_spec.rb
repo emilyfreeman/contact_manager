f731eca1c31d882c6e00bb765521c0d4b93a235d
Emily Dowdle <emily@editingemily.com>
2015-10-30 09:22:58 -0600

    updated phone number model to belong to person class

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe PhoneNumber, type: :model do
   let(:phone_number) { PhoneNumber.new(number: "1112223333", person_id: 1) }
 
+  it 'is associated with a person' do
+    expect(phone_number).to respond_to(:person)
+  end
+
   it 'is valid' do
     expect(phone_number).to be_valid
   end
