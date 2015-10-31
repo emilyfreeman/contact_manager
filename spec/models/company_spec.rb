7df0e5cafebc032402ac7bc09ec91fde1a0786fa
Emily Dowdle <emily@editingemily.com>
2015-10-31 16:18:51 -0600

    validation complete

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:company) { Company.new(name: "Dowdle Dynamics") }
+
+  it 'is valid' do
+    expect(company).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    company.name = nil
+    expect(company).to_not be_valid
+  end
+
 end
